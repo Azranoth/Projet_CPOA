70e3815213a579abf5c0da5a732b0717ea813157
Azranoth <awasmer96@yahoo.fr>
2017-11-08 13:39:41 +0100

    debug partiel Matrix33d

--- InterfaceCSG/matrix33d.cpp
+++ InterfaceCSG/matrix33d.cpp
@@ -1,31 +1,31 @@
 #include "matrix33d.h"
 
-Matrix33d::inversion(){
+Matrix33d Matrix33d::inversion(){
 
-    double det = this[0,0]*this[1,1]*this[2][2] +
-            this[0][1]*this[1][2]*this[2][0] +
-            this[0][2]*this[1][0]*this[2][1] -
-            this[0][2]*this[1][1]*this[2][0] -
-            this[0][0]*this[1][2]*this[2][1] -
-            this[0][1]*this[1][0]*this[2][2];
+    double det = matData[0][0]*matData[1][1]*matData[2][2] +
+            matData[0][1]*matData[1][2]*matData[2][0] +
+            matData[0][2]*matData[1][0]*matData[2][1] -
+            matData[0][2]*matData[1][1]*matData[2][0] -
+            matData[0][0]*matData[1][2]*matData[2][1] -
+            matData[0][1]*matData[1][0]*matData[2][2];
 
     if(det == 0){
         return nullptr;
     }
 
-    int tab_invert[3][3];
+    double tab_invert[3][3];
 
-    tab_invert[0][0] =  (M[1][1]*M[2][2] - M[1][2]*M[2][1]) / det;
-    tab_invert[1][0] = -(M[1][0]*M[2][2] - M[2][0]*M[1][2]) / det;
-    tab_invert[2][0] =  (M[1][0]*M[2][1] - M[2][0]*M[1][1]) / det;
+    tab_invert[0][0] =  (matData[1][1]*matData[2][2] - matData[1][2]*matData[2][1]) / det;
+    tab_invert[1][0] = -(matData[1][0]*matData[2][2] - matData[2][0]*matData[1][2]) / det;
+    tab_invert[2][0] =  (matData[1][0]*matData[2][1] - matData[2][0]*matData[1][1]) / det;
 
-    tab_invert[0][1] = -(M[0][1]*M[2][2] - M[2][1]*M[0][2]) / det;
-    tab_invert[1][1] =  (M[0][0]*M[2][2] - M[2][0]*M[0][2]) / det;
-    tab_invert[2][1] = -(M[0][0]*M[2][1] - M[2][0]*M[0][1]) / det;
+    tab_invert[0][1] = -(matData[0][1]*matData[2][2] - matData[2][1]*matData[0][2]) / det;
+    tab_invert[1][1] =  (matData[0][0]*matData[2][2] - matData[2][0]*matData[0][2]) / det;
+    tab_invert[2][1] = -(matData[0][0]*matData[2][1] - matData[2][0]*matData[0][1]) / det;
 
-    tab_invert[0][2] =  (M[0][1]*M[1][2] - M[1][1]*M[0][2]) / det;
-    tab_invert[1][2] = -(M[0][0]*M[1][2] - M[1][0]*M[0][2]) / det;
-    tab_invert[2][2] =  (M[0][0]*M[1][1] - M[1][0]*M[0][1]) / det;
+    tab_invert[0][2] =  (matData[0][1]*matData[1][2] - matData[1][1]*matData[0][2]) / det;
+    tab_invert[1][2] = -(matData[0][0]*matData[1][2] - matData[1][0]*matData[0][2]) / det;
+    tab_invert[2][2] =  (matData[0][0]*matData[1][1] - matData[1][0]*matData[0][1]) / det;
 
     Matrix33d invert = Matrix33d(tab_invert);
     return invert;
--- InterfaceCSG/matrix33d.h
+++ InterfaceCSG/matrix33d.h
@@ -4,39 +4,46 @@
 #include "Array.h"
 #include "vector.h"
 
-class Matrix33d : public Array<Vec3i,3> {
-
+class Matrix33d {
+protected:
+    Array<Array<double,3>,3> matData;
 public:
     // CONSTRUCTEURS
     Matrix33d(){
-        double input[3] = {double(0), double(0), double(0)};
         for(int i = 0; i < 3; i++){
-            this->data[i] = Array<Double,3>(input);
+            for(int j = 0; j < 3; j++){
+                this->matData[i][j] = double(0);
+            }
         }
     }
 
     Matrix33d(const double input[3][3]){
         for(int i = 0; i < 3; i++){
-            this->data[i] = Array<Double,3>(input[i]);
+            for(int j = 0; j < 3; j++){
+                this->matData[i][j] = input[i][j];
+            }
         }
     }
 
     Matrix33d(const Array<Array<double,3>,3> array){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                this->data[i].data[j] = array.data[i].data[j];
+                this->matData[i][j] = array[i][j];
             }
         }
     }
 
     // ACCESSEURS
-    double& operator[][](const int i, const int j){
-        return this->data[i].data[j];
+    Array<double,3>& operator[](const int i){
+        return this->matData[i];
     }
 
-    double operator[][](const int i, const int j) const{
-        return this->data[i].data[j];
+    Array<double,3> operator[](const int i) const{
+        return this->matData[i];
     }
+
+    // FONCTIONS
+    Matrix33d inversion();
 
 };
 #endif // MATRICE33D
