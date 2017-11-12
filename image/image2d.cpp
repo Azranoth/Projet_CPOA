876f19fb13c119f0595831abbf927ed79aa65214
Azranoth <awasmer96@yahoo.fr>
2017-11-12 21:08:17 +0100

    Classe Image2D - needs tests

--- image/image2d.cpp
+++ image/image2d.cpp
@@ -1,2 +1,27 @@
 #include "image2d.h"
-#include <matrix33d.h>
+
+template<typename T>
+void Image2D<T>::imgSwap(Image2D<T> img){
+    T temp_val;
+    for(int i = 0; i < _w; i++){
+        for(int j = 0; j < _h; j++){
+
+            temp_val = this(i,j);
+            setPixel(i,j,img(i,j));
+            img.setPixel(i,j, temp_val);
+
+        }
+    }
+}
+
+template<typename T>
+Image2D<T> Image2D<T>::crop(const int origin_x, const int origin_y, const int width, const int height){
+    Image2D<T> cropped_image = Image2D<T>(width, height);
+    for(int i = origin_x; i < origin_x+width; i++){
+        for(int j = origin_y; j < origin_y+height; j++){
+            cropped_image.setPixel((i - origin_x), (j-origin_y), this(i,j));
+        }
+    }
+    return cropped_image;
+}
+
--- image/image2d.h
+++ image/image2d.h
@@ -4,20 +4,60 @@
 #include "matrix33d.h"
 
 
-template<typename T, int W, int H>  //  W : width; H : Height
+template<typename T>
 class Image2D
 {
 protected:
-    T *imgData;
+    T *_imgData;
+    int _w;
+    int _h;
 public:
-    inline Image2D(const T input[W][H]){
-        imgData = new T[W][H];
-        for(int i = 0; i < W; i++){
-            for(int j = 0; j < H; j++){
-                imgData[i][j] = input[i][j];
+
+    // CONSTRUCTEURS
+    inline Image2D(int width, int height){
+        _w = width; _h = height;
+        _imgData = new T[_w*_h];
+        for(int i = 0; i < _w; i++){
+            for(int j = 0; j < _h; j++){
+                this(i,j) = T(0);
             }
         }
     }
+
+    inline Image2D(int width, int height, const T* input){
+        _w = width; _h = height;
+        _imgData = new T[_w*_h];
+        for(int i = 0; i < _w*_h; i++){
+            _imgData[i] = input[i];
+        }
+    }
+    // DESTRUCTEUR
+    ~Image2D(){
+        delete [] _imgData;
+    }
+
+    // OPERATEURS
+    T& operator()(const int x, const int y){
+        return _imgData[x+y*_w];
+    }
+    T operator()(const int x, const int y) const {
+        return _imgData[x+y*_w];
+    }
+
+    // ACCESSEURS
+    int width() const {return _w;}
+    int height() const {return _h;}
+    T* getData() const {return _imgData;}
+
+    // AFFECTATION
+    void setPixel(const int x, const int y, const T& value){
+        this(x,y) = value;
+    }
+
+    void imgSwap(Image2D<T> img);
+    Image2D<T> crop(const int origin_x, const int origin_y, const int width, const int height);
+
 };
+
 
 #endif // IMAGE2D_H
